56129c58c7a5a6a90a45a89c27cbe59ec5b604a6
Marcelo H <rengarmarcelo.mghgmail.comgit config --global user.email rengarmarcelo.mghgmail.com>
2024-07-21 10:11:06 -0300

    Criação do backend

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "bankdash_backend"
---
+++ src/main/kotlin/com/spring/bankdash_backend/BankdashBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.spring.bankdash_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BankdashBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<BankdashBackendApplication>(*args)
+}
